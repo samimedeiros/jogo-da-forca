c99d2e67d56d6691ee5107067adeaab107196938
samimedeiros <samiladesa@gmail.com>
2026-05-16 16:40:42 -0300

    Versão final do jogo da forca
    Foi adicionado a versão final do jogo da forca, onde temos como o jogo vai ser demonstrado, as decisões técnicas e também possíveis melhorias para o jogo

--- jogo-forca.py
+++ jogo-forca.py
@@ -1,2 +1,30 @@
 def iniciar_jogo():
     print("Jogo da Forca iniciado!")
+Versão final do projeto Jogo da Forca
+
+Demonstração do iniciar_jogo:
+        Tela Incial;
+        Escolha de letras;
+        Atualização da palavra escondida;
+        Contagem de tentativa;
+        Vitória e derrota;
+
+Durante a demonstração explicar como:
+        O utilizador interge;
+        O sistema valida as letras;
+        O jogo termina;
+
+Decisões técnicas:
+        Gestão de letras repetidas;
+        Utilização de lista para armazenar letras;
+        Validação de inputs;
+        Atualização do estado do jogo;
+
+Melhorias futuras:
+        Niveis de dificuldade;
+        Banco de palavras maiores;
+        Interface gráfica;
+        Sons e animação;
+        Pontuação;
+        Guardar o avanço do jogador;
+
